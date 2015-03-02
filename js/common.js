8c66b01798b57c33d32e8101a79094e04f7ed6f0
secofr <vadim.y.malinovsky@gmail.com>
2015-03-02 22:36:19 +0700

    Project The Best
    Landing Page для регионального  проекта Лучший в стиле живой книги Анна
    Каренина Live

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	function heightDetect() {
+		$(".main_head").css("height",$(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+});
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
